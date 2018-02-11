da160771290ea94cb95b73b8b0d60b32b1c1a26f
Pedro <pedro.resch@gmail.com>
2018-02-11 15:18:10 -0200

    Finished home page responsiveness

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
-var advancedVars = require('postcss-advanced-variables');
+var cssVars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
 var mixins = require('postcss-mixins');
@@ -9,7 +9,7 @@ var hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/style.css')
-  .pipe(postcss([cssImport, mixins, nested, advancedVars, hexrgba, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, nested, cssVars, hexrgba, autoprefixer]))
   .on('error', function(errorInfo){
     console.log(errorInfo.toString());
     this.emit('end');
